e80276bbe757bb1d0160531fbac27db05f31d60f
wgy <258024044@qq.com>
2021-04-13 23:01:40 +0800

    Initial commit

---
+++ src/com/qf/Demo1.java
@@ -0,0 +1,15 @@
+package com.qf;
+
+/**
+ * @author wgy
+ * @version V1.0
+ * @Project testgit001
+ * @Package com.qf
+ * @Description: ${TODO}
+ * @Date 2021/4/13 23:00
+ */
+public class Demo1 {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
